503da44c652b7ad1cf58dddafcd57604d3f7bb50
yba9285 <108516941+yba9285@users.noreply.github.com>
2025-01-17 18:54:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,13 +20,13 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/AoneMarathi") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002124208809'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/moviiezonerequest')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "MzFileStore")
+DATABASE_NAME = environ.get('DATABASE_NAME', "MzFileStore3")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Mz_Tg_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
