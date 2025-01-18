0e8130137ed22f85d0df03269760d943ec6ceab3
yba9285 <108516941+yba9285@users.noreply.github.com>
2025-01-18 13:50:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ START_IMG = (environ.get('START_IMG', 'https://envs.sh/o9V.jpg https://envs.sh/H
 FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
 PAYPICS = (environ.get('PAYPICS', 'https://graph.org/file/f4db1c3ad3d9e38b328e6.jpg')).split()
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/9f3f47c690bbcc67633c2.jpg'))
+SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://envs.sh/Hqi.jpg'))
 REACTIONS = ["👀", "😱", "🔥", "😍", "🎉", "🥰", "😇", "⚡"]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
