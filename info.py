013816808b0be1bef512d66a9522e3ce64956169
yba9285 <108516941+yba9285@users.noreply.github.com>
2025-01-21 13:47:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,7 +19,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1731356432').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/SamMarathi") # ADMIN USERNAME
+USERNAME = environ.get('USERNAME', "https://t.me/AoneMarathi") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002150303936'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/moviiezonerequest')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002124208809').split()]
